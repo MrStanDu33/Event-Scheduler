92cdfa9dba76cf99e5284c11f134cb334b84f10d
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2019-06-18 18:28:43 +0200

    changed die() with return

--- app.php
+++ app.php
@@ -1,5 +1,7 @@
 <?php
 	if (filter_input(INPUT_GET, "CORSProxy") != null && !empty(filter_input(INPUT_GET, "CORSProxy")))
+	{
 		echo(htmlspecialchars(file_get_contents(urldecode(filter_input(INPUT_GET, "CORSProxy")))));
-	die();
+		return;
+	}
 ?>
