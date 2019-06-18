d38ffab9e433ea6da6154b0e9cfc593ef70f2f00
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2019-06-18 18:16:46 +0200

    removed XSS Security Breach

--- app.php
+++ app.php
@@ -1,5 +1,5 @@
 <?php
 	if (isset($_GET["CORSProxy"]) && !empty($_GET["CORSProxy"]))
-		echo(file_get_contents(urldecode($_GET["CORSProxy"])));
+		echo(htmlspecialchars(file_get_contents(urldecode($_GET["CORSProxy"]))));
 	die();
 ?>
