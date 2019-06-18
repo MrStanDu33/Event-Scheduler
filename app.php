9a49341fa154363bdf81a9a3402a4f8cb9ea1e8b
DANIELS-ROTH Stan <contact@daniels-roth-stan.fr>
2019-06-18 18:28:02 +0200

    fixed some issues

--- app.php
+++ app.php
@@ -1,5 +1,5 @@
 <?php
 	if (filter_input(INPUT_GET, "CORSProxy") != null && !empty(filter_input(INPUT_GET, "CORSProxy")))
-		echo(htmlspecialchars(file_get_contents(urldecode($_GET["CORSProxy"]))));
+		echo(htmlspecialchars(file_get_contents(urldecode(filter_input(INPUT_GET, "CORSProxy")))));
 	die();
 ?>
